bd0c2fe967c20fedac1845e2e553dfe00c8df807
Lucas French <Lucas.french@nkompass.net>
2026-02-02 20:58:22 -0500

    CameraFollowScript
    I made a basic scene with a single square labeled 'Player'. Attached to the player is a script called PlayerMovement2D that allows movement using WASD. Attached to the camera is the CameraFollow2D script that follows the player around the scene from the top down view.

---
+++ VikingRealmsGame/Assets/Scripts/PlayerMovement2D.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerMovement2D : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = new Vector3(x, y, 0f).normalized;
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
