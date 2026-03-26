476cf87747bb5144913fb9b830862233460f87e7
Roosevelt James <142815674+RooJames@users.noreply.github.com>
2026-03-25 20:26:10 -0400

    Copy Initial Button
    copied over button scripts, animations, and canvas

---
+++ VikingRealmsGame/Assets/Scripts/ItemButtonController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ItemButtonController : MonoBehaviour
+{
+    public int ID;
+    private Animator anim;
+    public string itemName;
+    public TextMeshProUGUI itemText;
+    public Image selectedItem;
+    private bool selected = false;
+    public Sprite icon;
+
+    void Start()
+    {
+        
+        anim = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        if (selected)
+        {
+            selectedItem.sprite = icon;
+            itemText.text = itemName;
+        }
+    }
+
+    public void Selected()
+    {
+        selected = true;
+    }
+
+    public void Deselected()
+    {
+        selected = false;
+    }
+
+    public void HoverEnter()
+    {
+        anim.SetBool("Hover", true);
+        itemText.text = itemName;
+    }
+
+    public void HoverExit()
+    {
+        anim.SetBool("Hover", false);
+        itemText.text = "";
+    }
+}
